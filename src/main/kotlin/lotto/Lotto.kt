98dca59a1420ef6c19f132c2546b047f8a9bc534
todays_sun_day <178suna@gmail.com>
2026-01-03 14:11:33 +0900

    feat: 기본 로또 속성 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,10 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == numbers.distinct().size) { "[ERROR] 로또 번호는 중복될 수 없습니다." }
+        numbers.forEach { n ->
+            require(n in 1..45) { "[ERROR] 로또 번호는 1 ~ 45 사이여야 합니다." }
+        }
     }
 
     // TODO: 추가 기능 구현
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -11,7 +11,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
