8c8ad38d68b33800dd0456c232cc316976582c89
todays_sun_day <178suna@gmail.com>
2026-01-03 15:22:04 +0900

    docs: 구현할 기능 수정

---
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -0,0 +1,10 @@
+package lotto.model
+
+class WinningLotto(winningNumber: Lotto, bonusNumber: Int) {
+    init {
+        winningNumber.getNumbers().forEach { num ->
+            require(num != bonusNumber) { "[ERROR] 보너스번호가 당첨번호와 중복됩니다." }
+        }
+        require(bonusNumber in 1..45) { "[ERROR] 보너스번호가 1 ~ 45 범위를 벗어납니다." }
+    }
+}
